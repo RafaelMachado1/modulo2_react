d4f53fdfbbd70eb4f6835dab85feeec257f45b57
Rafael Machado <rafaelmartinsmachado25@gmail.com>
2025-04-21 19:29:52 -0300

    exercicios-desafio

--- exercicios_de_react/src/App.jsx
+++ exercicios_de_react/src/App.jsx
@@ -1,33 +1,79 @@
-import './App.css';
-import logo from "./img/logo.png";
 
+import './App.css';
 
 function App() {
 
-  const onClickBotao = () => {
-    alert("Botão foi clicado!");
+const titulo = "LabeTube"
+const tituloDoVideo = "Título do Vídeo"
 
+function reproduzVideo() {
+  alert("O vídeo está sendo reproduzido")
 }
 
 
-  return (  
-    <div className="container">
-        <h1>Aprenda React na Labenu!</h1>
-        <div>
-          <div>
-            <img className="Logo" src={logo} alt="logo"/>
-          </div>
-          <div>
-            <a href="https://labenu.com.br">Site da Labenu</a>
-          </div>
-          <div>
-            <button onClick={onClickBotao}> Clique aqui! </button>
-          </div>
-        </div>
-      </div>
 
+  return (
+    <div>
+       <div className="tela-inteira">
+        <header>
+            <h1>{titulo}</h1>
+            <input type="text" placeholder="Busca" id="campoDeBusca"/>
+        </header>
 
+        <main>
+            <nav className="menu-vertical">
+                <ul>
+                    <li className="botoes-meunu-vertical">Início</li>
+                    <li className="botoes-meunu-vertical">Em alta</li>
+                    <li class="botoes-meunu-vertical">Inscrições</li>
+                    <hr>
+                    </hr>
+                    <li className="botoes-meunu-vertical">Originais</li>
+                    <li className="botoes-meunu-vertical">Histórico</li>
+                </ul>
+            </nav>
 
+            <section className="painel-de-videos">
+                <div className="box-pagina-principal media1" onClick={() => reproduzVideo()}>
+                    <img src="https://picsum.photos/400/400?a=1 " alt=""/>
+                    <h4>{tituloDoVideo}</h4>
+                </div>
+                <div className="box-pagina-principal media2" onClick={() => reproduzVideo()}>
+                    <img src="https://picsum.photos/400/400?a=2 " alt=""/>
+                    <h4>{tituloDoVideo}</h4>
+                </div>
+                <div className="box-pagina-principal media3" onClick={() => reproduzVideo()}>
+                    <img src="https://picsum.photos/400/400?a=3 " alt=""/>
+                    <h4>{tituloDoVideo}</h4>
+                </div>
+                <div className="box-pagina-principal media4" onClick={() => reproduzVideo()}>
+                    <img src="https://picsum.photos/400/400?a=4 " alt=""/>
+                    <h4>{tituloDoVideo}</h4>
+                </div>
+                <div className="box-pagina-principal media5" onClick={() => reproduzVideo()}>
+                    <img src="https://picsum.photos/400/400?a=5 " alt=""/>
+                    <h4>{tituloDoVideo}</h4>
+                </div>
+                <div className="box-pagina-principal media6" onClick={() => reproduzVideo()}>
+                    <img src="https://picsum.photos/400/400?a=6 " alt=""/>
+                    <h4>{tituloDoVideo}</h4>
+                </div>
+                <div className="box-pagina-principal media7" onclick={() => reproduzVideo()}>
+                    <img src="https://picsum.photos/400/400?a=7 " alt=""/>
+                    <h4>{tituloDoVideo}</h4>
+                </div>
+                <div className="box-pagina-principal media8" onClick={() => reproduzVideo()}>
+                    <img src="https://picsum.photos/400/400?a=8 " alt=""/>
+                    <h4>{tituloDoVideo}</h4>
+                </div>
+            </section>
+        </main>
+
+        <footer>
+            <h4>Oi! Eu moro no footer!</h4>
+        </footer>
+    </div>
+    </div>
   );
 }
 
