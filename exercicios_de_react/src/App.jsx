8017bdfcd3311c851f07adcbbe91efc591e08ad1
Rafael Machado <rafaelmartinsmachado25@gmail.com>
2025-04-22 17:32:51 -0300

    exercicio-template

--- exercicios_de_react/src/App.jsx
+++ exercicios_de_react/src/App.jsx
@@ -1,80 +1,23 @@
+import React from "react";
+import Garagem from "./componentes/Garagem";
+import Footer from "./componentes/Footer";
+import Header from "./componentes/Header";
 
-import './App.css';
 
-function App() {
 
-const titulo = "LabeTube"
-const tituloDoVideo = "Título do Vídeo"
 
-function reproduzVideo() {
-  alert("O vídeo está sendo reproduzido")
-}
 
+
+export default function App() {
 
 
   return (
     <div>
-       <div className="tela-inteira">
-        <header>
-            <h1>{titulo}</h1>
-            <input type="text" placeholder="Busca" id="campoDeBusca"/>
-        </header>
+      <Header />
+      <h1>Aula - Componentes React</h1>
+      <Garagem />
+      <Footer />
 
-        <main>
-            <nav className="menu-vertical">
-                <ul>
-                    <li className="botoes-meunu-vertical">Início</li>
-                    <li className="botoes-meunu-vertical">Em alta</li>
-                    <li class="botoes-meunu-vertical">Inscrições</li>
-                    <hr>
-                    </hr>
-                    <li className="botoes-meunu-vertical">Originais</li>
-                    <li className="botoes-meunu-vertical">Histórico</li>
-                </ul>
-            </nav>
-
-            <section className="painel-de-videos">
-                <div className="box-pagina-principal media1" onClick={() => reproduzVideo()}>
-                    <img src="https://picsum.photos/400/400?a=1 " alt=""/>
-                    <h4>{tituloDoVideo}</h4>
-                </div>
-                <div className="box-pagina-principal media2" onClick={() => reproduzVideo()}>
-                    <img src="https://picsum.photos/400/400?a=2 " alt=""/>
-                    <h4>{tituloDoVideo}</h4>
-                </div>
-                <div className="box-pagina-principal media3" onClick={() => reproduzVideo()}>
-                    <img src="https://picsum.photos/400/400?a=3 " alt=""/>
-                    <h4>{tituloDoVideo}</h4>
-                </div>
-                <div className="box-pagina-principal media4" onClick={() => reproduzVideo()}>
-                    <img src="https://picsum.photos/400/400?a=4 " alt=""/>
-                    <h4>{tituloDoVideo}</h4>
-                </div>
-                <div className="box-pagina-principal media5" onClick={() => reproduzVideo()}>
-                    <img src="https://picsum.photos/400/400?a=5 " alt=""/>
-                    <h4>{tituloDoVideo}</h4>
-                </div>
-                <div className="box-pagina-principal media6" onClick={() => reproduzVideo()}>
-                    <img src="https://picsum.photos/400/400?a=6 " alt=""/>
-                    <h4>{tituloDoVideo}</h4>
-                </div>
-                <div className="box-pagina-principal media7" onclick={() => reproduzVideo()}>
-                    <img src="https://picsum.photos/400/400?a=7 " alt=""/>
-                    <h4>{tituloDoVideo}</h4>
-                </div>
-                <div className="box-pagina-principal media8" onClick={() => reproduzVideo()}>
-                    <img src="https://picsum.photos/400/400?a=8 " alt=""/>
-                    <h4>{tituloDoVideo}</h4>
-                </div>
-            </section>
-        </main>
-
-        <footer>
-            <h4>Oi! Eu moro no footer!</h4>
-        </footer>
-    </div>
     </div>
   );
 }
-
-export default App;
---
+++ exercicios_de_react/src/componentes/RedesSociais.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function RedesSociais(){
+
+
+    return(
+        <div>
+            
+        </div>
+      
+    )
+}
+
+export default RedesSociais;
