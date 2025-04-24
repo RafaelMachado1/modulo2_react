c139a2219e0a1595cedc5b891fe7b84773939f17
Rafael Machado <rafaelmartinsmachado25@gmail.com>
2025-04-23 21:33:15 -0300

    exercicio-template

--- exercicios_de_react/src/App.jsx
+++ exercicios_de_react/src/App.jsx
@@ -1,63 +1,17 @@
-import React from "react";
-import "./../src/App.css";
-import { CardVideo } from "./componentes/CardVideo";
+import { Garagem } from "./Componentes/Garagem/Garagem";
+import { GlobalStyled } from "./GlobalStyle";
 
 export default function App() {
-  const informacaoVideo = {
-    imagem: "https://picsum.photos/400/400?a=1 ",
-    titulo: "Título do vídeo"
+  const nome = "Labenu";
+
+  function apresentaGaragem() {
+    alert(`Boas vindas à garagem de ${nome}`);
   }
 
   return (
-
-    <div className="tela-inteira">
-      <header>
-        <h1>LabeTube</h1>
-        <input type="text" placeholder="Busca" id="campoDeBusca" />
-      </header>
-      <main>
-        <nav className="menu-vertical">
-          <ul>
-            <li className="botoes-meunu-vertical">Início</li>
-            <li className="botoes-meunu-vertical">Em alta</li>
-            <li className="botoes-meunu-vertical">Inscrições</li>
-            <hr />
-            <li className="botoes-meunu-vertical">Originais</li>
-            <li className="botoes-meunu-vertical">Histórico</li>
-          </ul>
-        </nav>
-        <section className="painel-de-videos">
-          <CardVideo
-            imagem={<img src={informacaoVideo.imagem} alt="" />}
-            tituloDoVideo={informacaoVideo.titulo} />
-          <CardVideo
-            imagem={<img src={informacaoVideo.imagem} alt="" />}
-            tituloDoVideo={informacaoVideo.titulo} />
-          <CardVideo
-            imagem={<img src={informacaoVideo.imagem} alt="" />}
-            tituloDoVideo={informacaoVideo.titulo} />
-          <CardVideo
-            imagem={<img src={informacaoVideo.imagem} alt="" />}
-            tituloDoVideo={informacaoVideo.titulo} />
-          <CardVideo
-            imagem={<img src={informacaoVideo.imagem} alt="" />}
-            tituloDoVideo={informacaoVideo.titulo} />
-          <CardVideo
-            imagem={<img src={informacaoVideo.imagem} alt="" />}
-            tituloDoVideo={informacaoVideo.titulo} />
-          <CardVideo
-            imagem={<img src={informacaoVideo.imagem} alt="" />}
-            tituloDoVideo={informacaoVideo.titulo} />
-          <CardVideo
-            imagem={<img src={informacaoVideo.imagem} alt="" />}
-            tituloDoVideo={informacaoVideo.titulo} />
-        </section>
-      </main>
-
-      <footer>
-        <h4>Oi! Eu moro no footer!</h4>
-      </footer>
+    <div className="App">
+      <GlobalStyled />
+      <Garagem nome={nome} mensagemAprentacao={apresentaGaragem} />
     </div>
-
   );
 }
